c9ab42607eef958672ad9bf655cf988706324a6d
George <george@wholewhale.com>
2025-08-29 14:04:22 -0700

    Update vite.config.ts
    update permission vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      server: {
+        host: '0.0.0.0',
+        port: 80,
+        allowedHosts: ['https://phpstack-1495536-5818998.cloudwaysapps.com/','https://138.197.82.237','causewriter.ai']
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
